720f6f513223ab9bbcf6017df354ced2622c9420
Alejandro4092 <acobomarcos@gmail.com>
2026-03-06 13:59:47 +0100

    Add project files.

---
+++ Models/Departamento.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcCorePaginacionRegistros.Models
+{
+    [Table("DEPT")]
+    public class Departamento
+    {
+        [Key]
+        [Column("DEPT_NO")]
+        public int IdDepartamento { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Column("DNOMBRE")]
+        public string Nombre { get; set; }
+
+        [Column("LOC")]
+        public string? Localidad { get; set; }
+
+        public ICollection<Empleado>? Empleados { get; set; }
+    }
+}
---
+++ Repositories/RepositoryHospital.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics.Metrics;
+using System.Threading;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using MvcCorePaginacionRegistros.Data;
+using MvcCorePaginacionRegistros.Models;
+
+namespace MvcCorePaginacionRegistros.Repositories
+{
+    #region vista
+    //    create or alter view V_DEPARTAMENTOS_INDIVIDUAL
+    //AS
+    //select cast(ROW_NUMBER() over (order by DEPT_NO)as int) as POSICION,DEPT_NO,DNOMBRE,LOC FROM DEPT
+    //GO
+    #endregion
+    public class RepositoryHospital
+    {
+        private HospitalContext context;
+
+        public RepositoryHospital(HospitalContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<int> GetNumeroRegistrosVistaDepartamentosAsync()
+        {
+            return await context.VistaDepartamentos.CountAsync();
+        }
+
+        public async Task<VistaDepartamento> GetVistaDepartamentoAsync(int posicion)
+        {
+            VistaDepartamento departamento = await this
+                .context.VistaDepartamentos.Where(z => z.Posicion == posicion)
+                .FirstOrDefaultAsync();
+            return departamento;
+        }
+
+        public async Task<List<VistaDepartamento>> GetGrupoVistaDepartamentoAsync(int posicion)
+        {
+            var consulta =
+                from datos in this.context.VistaDepartamentos
+                where datos.Posicion >= posicion && datos.Posicion < (posicion + 2)
+                select datos;
+            return await consulta.ToListAsync();
+        }
+    }
+}
